4e6240be58fe411ca60ce21e1077feb4703222fe
slavanorm <512f@bk.ru>
2020-03-19 00:45:39 +0100

    new ft

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 import unittest
 import time
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,19 +17,31 @@ class NewVisitorTest(unittest.TestCase):
             r"C:\Program Files\chromium\Bin\Chrome\chrome.exe"
         )
         self.browser = webdriver.Chrome(chrome_options=options)
+        self.browser.get("http://localhost:8000")
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id(
+                    "id_list_table"
+                )
+                rows = table.find_elements_by_tag_name("tr")
+                self.assertIn(
+                    row_text, [row.text for row in rows]
+                )
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get("http://localhost:8000")
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
@@ -51,7 +66,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table(
             "1: Buy peacock feathers"
         )
         # There is still a text box inviting her to add another item. She
@@ -65,10 +80,10 @@ class NewVisitorTest(unittest.TestCase):
         # The page updates again, and now shows both items on her list
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table(
             "1: Buy peacock feathers"
         )
-        self.check_for_row_in_list_table(
+        self.wait_for_row_in_list_table(
             "2: Use peacock feathers to make a fly"
         )
 
@@ -77,6 +92,55 @@ class NewVisitorTest(unittest.TestCase):
         # explanatory text to that effect.
 
         # She visits that URL - her to-do list is still there
+
+    def test_multiple_users_can_start_lists_at_different_urls(
+        self,
+    ):
+        # Edith starts a new list
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table(
+            "1: Buy peacock feathers"
+        )
+        # she notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/lists/.+")
+        # Now a new user, Francis, comes along to the site.
+
+        # meta/ we use a new browser session to make sure that no information
+        # meta/ of edith's is coming through cookies
+        self.tearDown()
+        self.setUp()
+
+        # Francis visits the home page.  There is no sign of Edith's
+        # list
+        page_text = self.browser.find_element_by_tag_name(
+            "body"
+        ).text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertNotIn("make a fly", page_text)
+
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/.+")
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name(
+            "body"
+        ).text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertIn("Buy milk", page_text)
+
+        # Satisfied, they both go back to sleep
 
 
 if __name__ == "__main__":
